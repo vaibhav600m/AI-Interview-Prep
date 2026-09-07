1047eb96d6e5ade19315eaf52ae77deebb3f553d
Vaibhav Mathur <vaibhav600m@email.com>
2026-09-07 12:25:25 +0530

    Add CORS for frontend deployment

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-
+from flask_cors import CORS
 from services.ai_service import ask_ai, client
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 # =========================
 # INTERVIEW MEMORY
